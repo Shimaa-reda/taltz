da1ca96fd50547bf1272c6646b63403c16207e25
Shimaa Reda <81780715+Shimaa-reda@users.noreply.github.com>
2025-02-11 12:32:49 +0200

    updates ref

--- js/index.js
+++ js/index.js
@@ -11,9 +11,12 @@ const questions = [
         label1: "50% improvement",
         label2: "100% improvement",
         type: "circles",
-        // color1: "red",  
         color1: "#49A942",  
         color2: "#49A942",
+        reference:["PsO=psoriasis; SD=Standard Deviation.","1.Gorelick J, et al. Dermatol Ther (Heidelb). 2019;9(4): 785–797. "
+            ,"2.Strober BE, et al. Dermatol Ther (Heidelb). 2019;9(1): 5–18."
+        ]
+       
         
     },
     { 
@@ -24,6 +27,9 @@ const questions = [
         image: "./images/q2.png",
         type: "circles",
         color1: "#49A942",  
+        reference:["1.Blauvelt A, Lebwohl MG, Mabuchi T, et al. Long-term efficacy and safety of ixekizumab: a 5-year analysis of the UNCOVER-3 randomized controlled trial. J Am Acad Dermatol. 2021;85:360-368.","2. Data on File. REF-65571. Eli Lilly and Company. 2020."
+            
+        ]
         
     },
     { 
@@ -38,7 +44,10 @@ const questions = [
         label2: "Almost complete skin clearance (PASI 90)",
         type: "persons",
         color1: "#49A942",  
-        color2: "#49A942"
+        color2: "#49A942",
+        reference:["1. Blauvelt A, Lebwohl MG, Mabuchi T, et al. Long-term efficacy and safety of ixekizumab: a 5-year analysis of the UNCOVER-3 randomized controlled trial.J Am Acad Dermatol. 2021;85:360-368.","2. Data on File. REF-65491. Eli Lilly and Company. 2020."
+            
+        ]
     },
     { 
         question: "In the PSoHO study, how would the durability* of Taltz compare to other treatment options like Risankizumab and Secukinumab ?", 
@@ -57,7 +66,9 @@ const questions = [
         color1: "#49A942",  
         color2: "#5370D9",
         color3: "#B89855",  
-        note:"* durability is defined as having a rapid and lasting response that was measured by % of patients achieving PASI 90 at week 12, and maintained it at week 24 and 52"
+        note:"* durability is defined as having a rapid and lasting response that was measured by % of patients achieving PASI 90 at week 12, and maintained it at week 24 and 52",
+        reference:["1. Pinter A, et al. DermatolTher(Heidelb). 2024;14(6):1479–1493."
+        ]
 
     },
     { 
@@ -72,7 +83,9 @@ const questions = [
         image: "./images/q5.png",
         type: "persons",
         color1: "#49A942",  
-        color2: "#6EBDC4"
+        color2: "#6EBDC4",
+        reference:["1. Blauvelt A, Leonardi C, Elewski B, et al. IXORA-R Study Group. A head-to-head comparison of ixekizumab vs. guselkumab in patients with moderate-to-severe plaque psoriasis: 24-week efficacy and safety results from a randomized, double-blinded trial. Br J Dermatol. 2021;184: 1047-1058. "
+        ]
     },
     { 
         question: "Out of 10 patients, how many experienced no radiographic changes over a 3-year journey with Taltz?", 
@@ -82,6 +95,9 @@ const questions = [
         image: "./images/q6.png",
         type: "persons",
         color1: "#49A942",  
+        reference:["1. Chandran V, van der Heijde D, Fleischmann RM, et al. lxekizumab treatment of biologic-naive patients with active psoriatic arthritis: 3-year results from a phase Ill clinical trial (SPIRIT-Pl). Rheumatology (Oxford). 2020;59:2774-2784. ","2. Mease PJ, van der Heijde D, Ritchlin CT, et al. SPIRIT-Pl Study Group. lxekizumab, an interleukin·l7A specific monoclonal antibody, for the treatment of biologic-naive patients with active psoriatic arthritis: results from the 24-week randomised, double-blind, placebo· controlled and active (adalimumab)·controlled period of the phase Ill trial SPI RIT·Pl. Ann Rheum Dis. 2017; 76: 79-87. "
+            ,"3. Haroon M, Gallagher P, FitzGerald 0. Diagnostic delay of more than 6 months contributes to poor radiographic and functional outcome in psoriatic arthritis. Ann Rheum Dis. 2015;74:1045-1050. "
+        ]
 
     }
 ];
@@ -343,9 +359,10 @@ document.getElementById('submit').addEventListener('click', function() {
     }
 
     if (currentQuestionIndex === 0) {
-        correctAnswerText += " Days"; // Append % if the range is from 0 to 100
+        correctAnswerText += " Days"; // Append Days if the range is from 0 to 60 or 120
     }
 
+    // Check additional answers if they exist
     if (currentQuestion.answer.length > 1) {
         const userAnswer2 = parseFloat(document.getElementById('range2').value);
         const correctAnswer2 = currentQuestion.answer[1];
@@ -356,6 +373,7 @@ document.getElementById('submit').addEventListener('click', function() {
             correctAnswerText += "%";
         }
     }
+    
     if (currentQuestion.answer.length > 2) {
         const userAnswer3 = parseFloat(document.getElementById('range3').value);
         const correctAnswer3 = currentQuestion.answer[2];
@@ -368,11 +386,15 @@ document.getElementById('submit').addEventListener('click', function() {
     }
 
     document.getElementById('modal-text').innerHTML = isCorrect 
-    ? '<span style="color: #49A942; font-weight:bold">Correct answer!</span> '
-    : `<span style="color: red; font-weight:bold">Oh!</span><br><br>
-       <span style="color: green;">${currentQuestionIndex === 0 ? correctAnswerText + " Days" : correctAnswerText}</span>`;
+        ? '<span style="color: #49A942; font-weight:bold">Correct answer!</span> '
+        : '<span style="color: red; font-weight:bold">Oh!</span><br><br>' +
+          `<span style="color: green;">${currentQuestionIndex === 0 ? correctAnswerText + " Days" : correctAnswerText}</span>`;
 
     document.getElementById('modal-img').src = isCorrect ? "./images/like.png" : "./images/sad.png";
+
+    // Display references if available
+    const referenceText = currentQuestion.reference.join('<br>');
+    document.getElementById('ref').innerHTML = referenceText;
 
     if (currentQuestion.image) {
         document.getElementById("modal-img2").src = currentQuestion.image;
